39acbd8b6408a9cc11204aa624bc0ac580f866a3
colinblake20 <cblake149@gmail.com>
2020-02-13 10:48:15 -0500

    delete old autonomi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/LiftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/LiftTest.java
@@ -17,11 +17,9 @@ public class LiftTest extends LinearOpMode {
     boolean intakeUp = false;
 
     public void runOpMode(){
-        robot.stoneArm.initialize(hardwareMap, telemetry, false);
+        robot.lift.initialize(hardwareMap, telemetry, false);
         robot.drivetrain.initialize(hardwareMap, telemetry, false);
 
-
-        robot.stoneArm.setBlockHingeDown();
 
 
         waitForStart();
@@ -31,52 +29,6 @@ public class LiftTest extends LinearOpMode {
 
         while(opModeIsActive()){
 
-            if(gamepad1.dpad_down){
-                downTimer.reset();
-            }
-            if(downTimer.seconds() < 0.25 && runTimer.seconds() > 0.26){
-                robot.stoneArm.setIntakePower(0.2);
-            } else {
-                if(gamepad1.dpad_up && !intakeChanged) {
-                    robot.stoneArm.intake.setPower(intakeUp ? -0.3 : 0); //this is a shorthand if/else statement, (condition ? if true : if false)
-                    intakeUp = !intakeUp;
-                    intakeChanged = true;
-                } else if(!gamepad1.dpad_up) {
-                    intakeChanged = false;
-                }
-            }
-
-
-            if(gamepad1.a){
-                robot.stoneArm.setBarHingeDown();
-                telemetry.addData("Bar Hinge: ", "down");
-            }
-            if(gamepad1.b){
-                robot.stoneArm.setBarHingeUp();
-                telemetry.addData("Bar Hinge: ", "up");
-            }
-
-            if(gamepad1.x){
-                robot.stoneArm.setBlockHingeDown();
-                telemetry.addData("Block Hinge: ", "down");
-            }
-            if(gamepad1.y){
-                robot.stoneArm.setBlockHingeUp();
-                telemetry.addData("Block Hinge: ", "up");
-            }
-
-            //linear lift
-            if(gamepad1.right_trigger > 0){
-                robot.stoneArm.basicLiftPower(gamepad1.right_trigger);
-            } else if(gamepad1.left_trigger > 0){
-                robot.stoneArm.basicLiftPower(-gamepad1.left_trigger);
-            } else {
-                robot.stoneArm.basicLiftPower(0);
-            }
-
-            //robot.drivetrain.setPower(gamepad1.left_stick_x, -gamepad1.left_stick_y, -gamepad1.right_stick_x);
-
-            telemetry.addData("Lift Position: ", robot.stoneArm.linearLift.getCurrentPosition());
 
             telemetry.update();
         }
